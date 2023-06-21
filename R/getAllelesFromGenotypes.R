89c417ac4b089bfdc2e0c257e59e26bd0f34fa63
Franco Marsico <franco.lmarsico@gmail.com>
2023-06-20 21:26:33 -0300

    Delete R directory

--- R/getAllelesFromGenotypes.R
+++
@@ -1,8 +0,0 @@
-#' @title getAllelesFromGenotypes
-#' @description Get alleles from genotypes
-#' @param g genotypes
-#' @return alleles
-#' @export
-getAllelesFromGenotypes<-function(g){
-   return(unique(unlist(strsplit(g,"/"))))
-}
